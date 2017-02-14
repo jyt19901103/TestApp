1856959c0de2e0cf8af53daedb3376353491b957
jyt19901103 <jiangyueting@ting-3.local>
2017-02-14 15:04:10 +0800

    群组动态获取数据

--- TestInterface/MainViewController.m
+++ TestInterface/MainViewController.m
@@ -35,16 +35,20 @@
 @property (copy, nonatomic) NSString *rest_url;
 
 
+
 @end
 
 @implementation MainViewController
+{
+    dispatch_queue_t _serialQueue;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     self.title = @"测试rest接口";
 
-    self.navigationItem.backBarButtonItem=[[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
+    self.navigationItem.backBarButtonItem =  [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"开始" style:UIBarButtonItemStylePlain target:self action:@selector(onBtmClick:)];
     
 }
@@ -86,7 +90,6 @@
     [self.navigationController pushViewController:dvc animated:YES];
 }
 
-
 - (void)onBtmClick:(UIButton *)button{
     if (_dataSource != nil) {
         [_dataSource removeAllObjects];
@@ -103,7 +106,7 @@
     
     _app_name = _resultData[@"app_name"];
     _org_name = _resultData[@"org_name"];
-    NSDictionary *dnsDict = _resultData[@"DNS"];
+    NSDictionary *dnsDict = _resultData[@"Dns"];
     _restModel = [UserModel modelWithDict:dnsDict];
     if (_dataSource == nil) {
         _dataSource = [[NSMutableArray alloc]init];
@@ -156,31 +159,37 @@
 
     _url = [NSString stringWithFormat:@"%@%@",URLSTRING,_restModel.api];
     NSLog(@"1111111111111111%@",_url);
+    
+    __weak typeof(self) weakSelf = self;
     [HttpRequest httpOtherRequest:_url  RequestType:_restModel.method Header:_restModel.header Parameters:_restModel.body WithSuccess:^(id result) {
+        __strong MainViewController *strSelf = self;
         NSString *tokenString = [result objectForKey:@"access_token"];
         NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
         [defaults setObject:[NSString stringWithFormat:@"Bearer %@",tokenString] forKey:@"token"];
         [defaults synchronize];
-        _restModel.result = result;
-        [_dataSource addObject:_restModel];
-        [_tableView reloadData];
-        // 创建队列
-        NSOperationQueue *queue = [[NSOperationQueue alloc] init];
-        // 创建操作
-        NSOperation *a = [NSBlockOperation blockOperationWithBlock:^{
-            NSArray *userArray = _resultData[@"Users"];
+        strSelf.restModel.result = result;
+        [strSelf.dataSource addObject:strSelf.restModel];
+        [strSelf.tableView reloadData];
+        //
+        
+        dispatch_async([strSelf serialQueue], ^{//把block中的任务放入串行队列中执行，这是第一个任务
+            [weakSelf requestUsersData:_resultData[@"Users"]];
 
-            [self requestUsersData:userArray];
-        }];
-        NSOperation *b = [NSBlockOperation blockOperationWithBlock:^{
-            NSArray *afterArray = _resultData[@"After"];
-
-            [self requestUsersData:afterArray];
-        }];
-        [b addDependency:a];
-        [queue addOperation:a];
-        [queue addOperation:b];
-
+        });
+        
+        dispatch_async([strSelf serialQueue], ^{
+            [weakSelf requestUsersData:_resultData[@"Chat"]];
+            
+        });
+        
+        dispatch_async([strSelf serialQueue], ^{
+            [weakSelf requestUsersData:_resultData[@"ChatGroup"]];
+            
+        });
+        dispatch_async([strSelf serialQueue], ^{
+            [weakSelf requestUsersData:_resultData[@"ChatRoom"]];
+            
+        });
 
     } failure:^(NSError *error) {
         if (error) {
@@ -190,7 +199,10 @@
         _restModel.staus = [NSString stringWithFormat:@"%ld", statusCode];
     }];
 }
+
 - (void)requestUsersData:(NSArray *)userArray{
+  //  dispatch_queue_t concurrentQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+
     for (NSDictionary *dict in userArray) {
         UserModel *listModel = [UserModel modelWithDict:dict];
 
@@ -198,9 +210,19 @@
             NSString *str = [listModel.api stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
             _url = [NSString stringWithFormat:@"%@%@",URLSTRING,str];
         }
+       else if ([listModel.api rangeOfString:@"%1$s"].location !=NSNotFound) {
+            
+            _url = [NSString stringWithFormat:@"%@%@",URLSTRING,[listModel.api stringByReplacingOccurrencesOfString:@"%1$s" withString:GroupId]];
+        }
+       else if ([listModel.api rangeOfString:@"%2$s"].location !=NSNotFound) {
+            
+            _url = [NSString stringWithFormat:@"%@%@",URLSTRING,[listModel.api stringByReplacingOccurrencesOfString:@"%2$s" withString:ChatRoomId]];
+        }
         else{
             _url = [NSString stringWithFormat:@"%@%@",URLSTRING,listModel.api];
         }
+        NSLog(@"=============%@",_url);
+
         if (listModel.body != nil) {
             _requestBody  = [dict objectForKey:@"body"];
         }else{
@@ -213,22 +235,60 @@
             _requestHeader = nil;
         }
         
-        [HttpRequest httpRequest:_url RequestType:listModel.method Header:_requestHeader Parameters:_requestBody WithSuccess:^(id result) {
-            listModel.result = result;
-        } failure:^(NSError *error) {
-            if (error) {
-                NSLog(@"error: %@",error);
-                listModel.error = error;
-            }
-        } statusCode:^(NSInteger statusCode) {
-            listModel.staus = [NSString stringWithFormat:@"%ld", statusCode];
-            [self.tableView reloadData];
-
-        }];
-        [_dataSource addObject:listModel];
+//        dispatch_sync(concurrentQueue, ^{
+        //_url = [_url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        
+            [HttpRequest httpRequest:_url RequestType:listModel.method Header:_requestHeader Parameters:_requestBody WithSuccess:^(id result) {
+                listModel.result = result;
+                NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+                
+                if ([listModel.name isEqualToString:@"获取所有群组"]) {
+                    
+                    for (NSDictionary *dict in result[@"data"]) {
+                        // NSLog(@"======%@",dict);
+                        NSString *groupid = dict[@"groupid"];
+                        [defaults setObject:groupid forKey:@"GroupId"];
+                        [defaults synchronize];
+                    }
+                }
+                
+                if ([listModel.name isEqualToString:@"获取APP下所有聊天室"]) {
+                    
+                    for (NSDictionary *dict in result[@"data"]) {
+                        NSLog(@"%@",dict);
+                        NSString *chatRoomId = dict[@"id"];
+                        [defaults setObject:chatRoomId forKey:@"ChatRoomId"];
+                        [defaults synchronize];
+                    }
+                }
+            } failure:^(NSError *error) {
+                if (error) {
+                    NSLog(@"error: %@",error);
+                    listModel.error = error;
+                }
+            } statusCode:^(NSInteger statusCode) {
+                listModel.staus = [NSString stringWithFormat:@"%ld", statusCode];
+                
+              //  dispatch_async(dispatch_get_main_queue(), ^{
+                    [self.tableView reloadData];
+                    
+             //   });
+                
+            }];
+            [_dataSource addObject:listModel];
+            
+      //  });
 
     }
     
     [self.tableView reloadData];
 }
+- (dispatch_queue_t)serialQueue
+{
+    if (!_serialQueue) {
+        _serialQueue = dispatch_queue_create("serialQueue", DISPATCH_QUEUE_SERIAL);//创建串行队列
+    }
+    return _serialQueue;
+}
+
 @end
