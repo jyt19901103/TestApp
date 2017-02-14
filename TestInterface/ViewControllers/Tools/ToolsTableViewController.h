080a494dba8a0aa1a90ac4942b6cd16de29eba1a
dujiepeng <347302029@qq.com>
2017-02-14 17:03:27 +0800

    整理目录

---
+++ TestInterface/ViewControllers/Tools/ToolsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ToolsTableViewController.h
+//  TestInterface
+//
+//  Created by 杜洁鹏 on 14/02/2017.
+//  Copyright © 2017 JYT. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface ToolsTableViewController : BaseTableViewController
+
+@end
---
+++ TestInterface/ViewControllers/Tools/ToolsTableViewController.m
@@ -0,0 +1,35 @@
+//
+//  ToolsTableViewController.m
+//  TestInterface
+//
+//  Created by 杜洁鹏 on 14/02/2017.
+//  Copyright © 2017 JYT. All rights reserved.
+//
+
+#import "ToolsTableViewController.h"
+
+@interface ToolsTableViewController ()
+
+@end
+
+@implementation ToolsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
